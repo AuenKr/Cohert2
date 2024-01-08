48f1a7975e03475c8b25dad08228985c4f8c5126
AuenKr <auenkumar64@gmail.com>
2024-01-08 18:51:40 +0530

    week3 Mongo completed

--- assignments/week-3/03-mongo/db/index.js
+++ assignments/week-3/03-mongo/db/index.js
@@ -1,27 +1,52 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const { string } = require("yargs");
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect("mongodb://127.0.0.1:27017/courseApp");
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        min: [5, "Min 5 length"],
+    },
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        min: [5, "Min 5 length"],
+    },
+    course: [String],
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title: String,
+    description: String,
+    price: Number,
+    imageLink: URL,
+    published: Boolean,
 });
 
-const Admin = mongoose.model('Admin', AdminSchema);
-const User = mongoose.model('User', UserSchema);
-const Course = mongoose.model('Course', CourseSchema);
+const Admin = mongoose.model("Admin", AdminSchema);
+const User = mongoose.model("User", UserSchema);
+const Course = mongoose.model("Course", CourseSchema);
 
 module.exports = {
     Admin,
     User,
-    Course
-}
+    Course,
+};
--- assignments/week-3/03-mongo/routes/admin.js
+++ assignments/week-3/03-mongo/routes/admin.js
@@ -3,15 +3,32 @@ const adminMiddleware = require("../middleware/admin");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post("/signup", (req, res) => {
     // Implement admin signup logic
+    const { username, password } = req.body;
+    const isValid = Admin.find({ username: username });
+    if (!isValid) {
+        const data = new Admin({
+            username: username,
+            password: password,
+        });
+        data.save().then(
+            res.json({
+                message: "Admin created successfully",
+            })
+        );
+    } else {
+        res.status.json({
+            message: "username alredy exist",
+        });
+    }
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post("/courses", adminMiddleware, (req, res) => {
     // Implement course creation logic
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
+router.get("/courses", adminMiddleware, (req, res) => {
     // Implement fetching all courses logic
 });
 
