5c332915dbfc4b6d430fe1ab2c5ce5657cd7a3b5
AuenKr <103646877+AuenKr@users.noreply.github.com>
2024-02-10 08:11:32 +0530

    Update utils.ts

--- Week10/100xdev/week-13-postgres/src/utils.ts
+++ Week10/100xdev/week-13-postgres/src/utils.ts
@@ -1,7 +1,7 @@
 import { Client } from 'pg';
 
 export async function getClient() {
-    const client = new Client("postgres://fdkfdcjk:bwpqGgUAL0p1G31NfIAHaJ4n6ExY4IcJ@jelani.db.elephantsql.com/fdkfdcjk");
+    const client = new Client("Your Postgress connection string");
     await client.connect();
     return client;
 }
