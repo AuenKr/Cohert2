95a57df5a03e1215dc542e01cd0b9c67a658f832
Golden Kumar <auenkumar64@gmail.com>
2025-02-24 23:39:54 +0530

    added design Pattern

--- go-lang/main.go
+++ go-lang/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("Hello World")
+
+	time := time.Now()
+
+	weekday := time.Weekday()
+	fmt.Println(time)
+	fmt.Println(weekday)
 }
